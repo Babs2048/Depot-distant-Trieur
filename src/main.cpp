9d9049d896dbe128a75ccca98829db5a019e71d5
babs2048 <cissbabacar227@gmail.com>
2026-01-26 12:27:25 +0100

    Version lecture BP et pot

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,29 @@ void setup() {
   // Initialise l'écran LCD
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
+  lcd.setRGB(255,255,0); // Rétroéclairage Rouge
   lcd.printf("Trieur de balles");
+  pinMode(0, INPUT_PULLUP);
+  pinMode(2,INPUT_PULLUP);
+  pinMode(12,INPUT_PULLUP);
+
+
 
 }
 
 void loop() {
+int BP0=digitalRead(0);
+int BP1=digitalRead(2);
+int BP2=digitalRead(12);
+int Pot=analogRead(33);
+
+Serial.print("Pot=");
+Serial.println(Pot);
+
+lcd.setCursor(0,1);
+lcd.print("Pot=");
+lcd.print(Pot);
+lcd.print("");
+  
 
 }
