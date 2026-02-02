83583eb919437de993d31a7db319f5dc99781a60
Babs2048 <cissbabacar227@gmail.com>
2026-02-02 10:51:54 +0100

    Controle Hacheur

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include "rgb_lcd.h"
 
 rgb_lcd lcd;
+int pwm = 27;
+int frequence = 25000;
+int canal = 0;
+int resolution = 11;
+
 
 void setup() {
   // Initialise la liaison avec le terminal
@@ -15,7 +20,10 @@ void setup() {
   pinMode(0, INPUT_PULLUP);
   pinMode(2,INPUT_PULLUP);
   pinMode(12,INPUT_PULLUP);
-
+  pinMode(26, OUTPUT);
+  ledcSetup(canal, frequence, resolution);
+  ledcAttachPin(pwm, canal);
+  ledcWrite(canal, 0);
 
 
 }
@@ -26,13 +34,22 @@ int BP1=digitalRead(2);
 int BP2=digitalRead(12);
 int Pot=analogRead(33);
 
+
 Serial.print("Pot=");
 Serial.println(Pot);
+
+if (BP0 == 0) { // bouton appuyé (car PULLUP)
+   digitalWrite(26, HIGH); // active la sortie 
+   } 
+else { digitalWrite(26, LOW); // désactive la sortie 
+  }
+
 
 lcd.setCursor(0,1);
 lcd.print("Pot=");
 lcd.print(Pot);
 lcd.print("");
-  
+int duty = map(Pot, 0, 4095, 0, 2047); 
+ledcWrite(canal, duty);
 
 }
